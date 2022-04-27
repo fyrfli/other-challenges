076fd3af32f03aa301f4313cc465c95a285abe50
fyrfli <camille.frantz@me.com>
2022-04-26 23:15:29 -0500

    Completed the tabs challenge

--- tabs/scripts/script.js
+++ tabs/scripts/script.js
@@ -2,19 +2,53 @@
 const todayDate = new Date();
 jsyear.innerText = todayDate.getFullYear();
 
-// function for feather icons
-// feather.replace();
-
 //style switcher code
 const summaryTab = document.getElementById("Summary");
-const statsTab = document.getElementById("Stats");
+const specsTab = document.getElementById("Specs");
 const featuresTab = document.getElementById("Features");
 const refundsTab = document.getElementById("Refunds");
 
-const tabLinks = document.querySelectorAll(".tab__item");
+const tabLinks = document.querySelectorAll(".nav-item");
 
-console.log(tabLinks);
+tabLinks.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    for (i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].classList.remove("nav-active");
+    }
+    if (e.currentTarget.classList.contains("summary")) {
+      summaryTab.classList.remove("inactive");
+      specsTab.classList.add("inactive");
+      featuresTab.classList.add("inactive");
+      refundsTab.classList.add("inactive");
+      e.currentTarget.classList.add("nav-active");
+    } else if (e.currentTarget.classList.contains("specs")) {
+      summaryTab.classList.add("inactive");
+      specsTab.classList.remove("inactive");
+      featuresTab.classList.add("inactive");
+      refundsTab.classList.add("inactive");
+      e.currentTarget.classList.add("nav-active");
 
-tabLinks.forEach() {
-  console.log(this);
-};
+    } else if (e.currentTarget.classList.contains("feat")) {
+      summaryTab.classList.add("inactive");
+      specsTab.classList.add("inactive");
+      featuresTab.classList.remove("inactive");
+      refundsTab.classList.add("inactive");
+      e.currentTarget.classList.add("nav-active");
+
+    } else if (e.currentTarget.classList.contains("refund")) {
+      summaryTab.classList.add("inactive");
+      specsTab.classList.add("inactive");
+      featuresTab.classList.add("inactive");
+      refundsTab.classList.remove("inactive");
+      e.currentTarget.classList.add("nav-active");
+
+    } else {
+      summaryTab.classList.remove("inactive");
+      specsTab.classList.add("inactive");
+      featuresTab.classList.add("inactive");
+      refundsTab.classList.add("inactive");
+      e.currentTarget.classList.add("nav-active");
+
+    }
+  })
+});
