9dd6df14f30ed9329cb3ca6793e5d47803c055a3
fyrfli <camille.frantz@me.com>
2022-04-27 11:39:47 -0500

    "minified" some logic
    instead of individually setting each tab inactive within the if-then-else block, set them all in the for loop that applies styling to the tab.

--- tabs/scripts/script.js
+++ tabs/scripts/script.js
@@ -9,46 +9,32 @@ const featuresTab = document.getElementById("Features");
 const refundsTab = document.getElementById("Refunds");
 
 const tabLinks = document.querySelectorAll(".nav-item");
+const tabContent = document.querySelectorAll(".tab-item");
 
 tabLinks.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
     for (i = 0; i < tabLinks.length; i++) {
       tabLinks[i].classList.remove("nav-active");
+      tabContent[i].classList.add("inactive");
     }
     if (e.currentTarget.classList.contains("summary")) {
-      summaryTab.classList.remove("inactive");
-      specsTab.classList.add("inactive");
-      featuresTab.classList.add("inactive");
-      refundsTab.classList.add("inactive");
       e.currentTarget.classList.add("nav-active");
+
     } else if (e.currentTarget.classList.contains("specs")) {
-      summaryTab.classList.add("inactive");
       specsTab.classList.remove("inactive");
-      featuresTab.classList.add("inactive");
-      refundsTab.classList.add("inactive");
       e.currentTarget.classList.add("nav-active");
 
     } else if (e.currentTarget.classList.contains("feat")) {
-      summaryTab.classList.add("inactive");
-      specsTab.classList.add("inactive");
       featuresTab.classList.remove("inactive");
-      refundsTab.classList.add("inactive");
       e.currentTarget.classList.add("nav-active");
 
     } else if (e.currentTarget.classList.contains("refund")) {
-      summaryTab.classList.add("inactive");
-      specsTab.classList.add("inactive");
-      featuresTab.classList.add("inactive");
       refundsTab.classList.remove("inactive");
       e.currentTarget.classList.add("nav-active");
 
     } else {
       summaryTab.classList.remove("inactive");
-      specsTab.classList.add("inactive");
-      featuresTab.classList.add("inactive");
-      refundsTab.classList.add("inactive");
       e.currentTarget.classList.add("nav-active");
-
     }
   })
 });
